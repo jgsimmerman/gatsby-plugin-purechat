b237ef0f9952f5064b83a78e43d62f887d03edba
Markus Oehler <m.oehler@digital-city.solutions>
2019-04-27 16:45:56 +0200

    - Added prettier and configuration  - Setup a pre-commit hook

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 exports.onRenderBody = function({ setPostBodyComponents }, pluginOptions) {
-
-  const { websiteId, enabled } = pluginOptions;
+  const { websiteId, enabled } = pluginOptions
 
   if (!enabled) {
-    return null;
+    return null
   }
 
   return setPostBodyComponents([
@@ -14,10 +13,8 @@ exports.onRenderBody = function({ setPostBodyComponents }, pluginOptions) {
       type="text/javascript"
       data-cfasync="false"
       dangerouslySetInnerHTML={{
-        __html: `window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '${websiteId}', f: true }); done = true; } }; })();`
+        __html: `window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '${websiteId}', f: true }); done = true; } }; })();`,
       }}
     />,
-  ]);
-};
-
-
+  ])
+}
